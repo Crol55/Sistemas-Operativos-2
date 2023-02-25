282e92ee64349fbdc85771fba0afcb362541f57b
Carlos Orantes <carlosorantes77@gmail.com>
2023-02-24 19:11:36 -0600

    Se agrego chartjs y ahora ya se puede graficar correctamente

--- practica1/frontend/src/main.js
+++ practica1/frontend/src/main.js
@@ -4,9 +4,11 @@ import './app.css';
 import logo from './assets/images/logo-universal.png';
 import {GetDiskUsage,GetCPUUsage, Greet} from '../wailsjs/go/main/App';
 
+/*<div id="app"></div>
+    background-color: rgba(27, 38, 54, 1);
 document.querySelector('#app').innerHTML = `
     <img id="logo" class="logo">
-      <div class="result" id="result">Please enter your name below 👇</div>
+    <div class="result" id="result">Please enter your name below 👇</div>
       <div class="input-box" id="input">
         <input class="input" id="name" type="text" autocomplete="off" />
         <button class="btn" onclick="greet()">Greet</button>
@@ -18,7 +20,7 @@ document.getElementById('logo').src = logo;
 let nameElement = document.getElementById("name");
 nameElement.focus();
 let resultElement = document.getElementById("result");
-
+*/
 window.getRam = ()=>{
     GetCPUUsage().then(res =>{
         window.alert(Math.round(res));
@@ -31,6 +33,9 @@ window.getDisk = function (){
     })
 }
 
+window.test = function(){
+    window.alert("si deberia funcionar");
+}
 // Setup the greet function
 window.greet = function () {
     // Get name
@@ -53,3 +58,45 @@ window.greet = function () {
         console.error(err);
     }
 };
+
+const segundos = Array.from(Array(60).keys());
+
+    const data = [
+      { year: 2010, count: 40 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+    
+    var grafico = new Chart(
+      document.getElementById('acquisitions'),
+      {
+        type: 'line',
+        data: {
+          labels: segundos,
+          datasets: [
+            {
+              label: 'Acquisitions by year',
+              data: data.map(row => row.count)
+            }
+          ]
+        }
+      }
+    );
+
+    setTimeout(manejador, 3000);
+
+    function manejador(){
+        
+        grafico.data.labels.push(2017);
+        grafico.data.datasets[0].data.push(70);
+        grafico.update();
+    }
+
+console.log( segundos);
+
+    
+
