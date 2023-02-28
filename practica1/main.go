61621f4c90f10cc20a36eb004c747b10f60247e4
Carlos Orantes <carlosorantes77@gmail.com>
2023-02-27 18:07:51 -0600

    Se cambio el nombre de la app a proyecto1

--- practica1/main.go
+++ practica1/main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "practica1",
+		Title:  "proyecto1",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
